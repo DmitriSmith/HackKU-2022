1201d3e5122990c6fb6cfea6d04429f17df9adfb
Dima S <dmitrismith5@gmail.com>
2022-04-09 16:43:48 -0500

    Added ADSR envelope

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+class ADSR {
+    constructor (atk, dec, sus, rel) {
+        this.attack = atk;
+        this.decay = dec;
+        this.sustain = sus;
+        this.release = rel;
+    }
+}
+
+
 var AudioContext = window.AudioContext ||
     window.webkitAudioContext;
 
@@ -6,21 +16,40 @@ const context = new AudioContext;
 const master_volume = context.createGain();
 const waveforms = document.getElementsByName('waveform');
 
+
 //Buttons
 const start_button  = document.querySelector('#start');
-const stop_button   = document.querySelector('#stop');
-const volume_slider = document.querySelector('#volume-control')
+
+//Sliders
+const volume_slider  = document.querySelector('#volume-control');
+const attack_slider  = document.querySelector('#attack-control');
+const decay_slider   = document.querySelector('#decay-control');
+const sustain_slider = document.querySelector('#sustain-control');
+const release_slider = document.querySelector('#release-control');
 
 //Initialization
+const envelope = new ADSR(parseFloat(attack_slider.value),
+    parseFloat(decay_slider.value),
+    parseFloat(sustain_slider.value),
+    parseFloat(release_slider.value));
 master_volume.connect(context.destination);
-master_volume.gain.value = 0.1;
+master_volume.gain.value = parseFloat(volume_slider.value);
 let waveform;
-
-volume_slider.addEventListener("input", changeVolume);
-
-function changeVolume() {
+volume_slider.addEventListener('input', function(){
     master_volume.gain.value = this.value;
-}
+});
+attack_slider.addEventListener('input', function(){
+    envelope.attack = (parseFloat(this.value));
+});
+decay_slider.addEventListener('input', function(){
+    envelope.decay = (parseFloat(this.value));
+});
+sustain_slider.addEventListener('input', function(){
+    envelope.sustain = (parseFloat(this.value));
+});
+release_slider.addEventListener('input', function(){
+    envelope.release = (parseFloat(this.value));
+});
 
 function setWaveform() {
     for(let i = 0; i < waveforms.length; i++){
@@ -31,21 +60,27 @@ function setWaveform() {
 }
 
 start_button.addEventListener('click', function() {
-    const oscillator = context.createOscillator();
-    oscillator.frequency.setValueAtTime(220, 0);
-    oscillator.connect(master_volume);
-    oscillator.start(0);
-    oscillator.type = waveform;
-    stop_button.addEventListener('click', function() {
-        oscillator.stop(0);
-        delete oscillator;
-    });
+    let osc = context.createOscillator();
+    let note_gain = context.createGain();
+
+    note_gain.gain.setValueAtTime(0,0);
+    note_gain.gain.linearRampToValueAtTime(envelope.sustain, context.currentTime + envelope.attack);
+    note_gain.gain.setValueAtTime(envelope.sustain, context.currentTime + 1 - envelope.release);
+    note_gain.gain.linearRampToValueAtTime(0, context.currentTime+1);
+
+    osc.frequency.setValueAtTime(220, 0);
+
+    osc.connect(note_gain);
+    osc.start(0);
+    osc.stop(context.currentTime + 1);
+    osc.type = waveform;
     waveforms.forEach((waveformInput) => {
         waveformInput.addEventListener('change', function() {
             setWaveform();
-            oscillator.type = waveform;
+            osc.type = waveform;
         });
     });
+    note_gain.connect(master_volume);
 });
 
 
